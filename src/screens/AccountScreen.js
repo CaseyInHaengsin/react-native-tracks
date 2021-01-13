b2dfd8765b9ba48026a567d12556aa21ade2c739
CaseyInHaengsin <casey.rowley@kuali.co>
2021-01-13 06:23:09 -0700

    Added react native elements.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,9 +4,10 @@ import { View, StyleSheet, Text } from 'react-native';
 
 const AccountScreen = () => {
   return (
-    <View>
+      <>
       <Text style={{fontSize: 48}}>Account Screen</Text>
-    </View>
+      </>
+    
   )
 }
 
