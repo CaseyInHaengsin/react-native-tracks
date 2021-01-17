8bfcc200b2bf1cce1521d48bc8b0efbc75b107f9
CaseyInHaengsin <casey.rowley@kuali.co>
2021-01-16 21:37:44 -0700

    added react-native-elements and created auth provider.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,62 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button, Icon } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  
   return (
-    <View>
-      <Text style={{fontSize: 48}}>Signup Screen</Text>
-      <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}/>
-      <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')}/>
-    </View>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Signup Screen</Text>
+      </Spacer>
+      <Input label="Email"
+        value={email} 
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        rightIcon={
+          <Icon 
+            type="feather"
+            name="eye"
+          />
+        }
+      />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+      
+      </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+
+  }
   
 })
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
 
 export default SignupScreen;
 
--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -15,4 +15,3 @@ const styles = StyleSheet.create({
 })
 
 export default TrackDetailScreen;
-
