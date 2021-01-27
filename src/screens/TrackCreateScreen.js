36dbcedf8592f4ff0fdef9165213aed40e6f7db4
CaseyInHaengsin <casey.rowley@kuali.co>
2021-01-26 20:03:42 -0700

    mocking location; addeduseLocation hook, and am working on tracking when users navigate away from screen.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,27 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import '../_mockLocation';
+import React, { useContext } from 'react';
 import { Text } from 'react-native-elements';
 import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync } from 'expo-location';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
-const [err, setErr] = useState(null);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
+const TrackCreateScreen = ({ isFocused }) => {
+  const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(addLocation);
+  console.log(isFocused)
 
-  useEffect(() => {
-    startWatching();
-  }, [])
   return (
     <SafeAreaView forceInset={{ top: 'always'}}>
       <Text h3>Create a track</Text>
@@ -36,4 +27,4 @@ const styles = StyleSheet.create({
 })
 
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen)
